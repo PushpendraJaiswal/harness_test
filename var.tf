8ed83e6578c8406cc612470baef0e46b0d812d7e
PushpendraJaiswal <pushpendra31@yahoo.com>
2023-01-12 18:29:08 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -5,3 +5,13 @@ variable "region" {
 variable "instance_type" {
   default = "t2.micro"
 }
+
+variable "ami_linux" {
+  description = "The id of the AMAZON Linux 2 machine image (AMI) to use for the server."
+  default     = "ami-05fa00d4c63e32376"
+}
+
+variable "ami_win" {
+  description = "The id of the Windows machine image (AMI) to use for the server."
+  default     = "ami-0fb5befc1450ca205"
+}
